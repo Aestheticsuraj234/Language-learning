93ab139692f7f7e72fb742e3678934cedd66eba1
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-03-27 12:37:37 +0530

    Update layout and sidebar components

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,8 +1,18 @@
+import { FeedWrapper } from "@/components/wrappers/feed-wrapper";
+import { StickyWrapper } from "@/components/wrappers/sticky-wrapper";
+
 const LearnPage = ()=>{
 
     return(
-        <div>
-            Learn Page
+        <div className="flex flex-row-reverse gap-[48px] px-6">
+            <StickyWrapper>
+                hello
+            </StickyWrapper>
+
+<FeedWrapper>
+    Feed Wrapper
+</FeedWrapper>
+
         </div>
     )
 
