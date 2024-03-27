bcc22f8d028c02edb2fab12e72bb7ebbe31f308b
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-03-27 13:47:17 +0530

    Update .env and package.json files, add UserProgress component to LearnPage

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,21 +1,26 @@
 import { FeedWrapper } from "@/components/wrappers/feed-wrapper";
 import { StickyWrapper } from "@/components/wrappers/sticky-wrapper";
+import { Header } from "./_components/header";
+import { UserProgress } from "@/components/global/user-progress";
 
-const LearnPage = ()=>{
+const LearnPage = () => {
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+        activeCourse={{title:"Spanish",imageSrc:"/es.svg"}}
+        hasActiveSubscription={true}
+        hearts={5}
+        points={100}
 
-    return(
-        <div className="flex flex-row-reverse gap-[48px] px-6">
-            <StickyWrapper>
-                hello
-            </StickyWrapper>
+        />
+      </StickyWrapper>
 
-<FeedWrapper>
-    Feed Wrapper
-</FeedWrapper>
-
-        </div>
-    )
-
-}
+      <FeedWrapper>
+       <Header title="Spanish" />
+      </FeedWrapper>
+    </div>
+  );
+};
 
 export default LearnPage;
